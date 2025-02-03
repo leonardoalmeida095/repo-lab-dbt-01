7b0b8f2a7e23928b9c4adbe593c24df415ecf0d4
leonardo Souza <leonardo.souza0995@gmail.com>
2025-02-03 14:10:57 +0000

    implementação de testes

--- models/analise_vendas.sql
+++ models/analise_vendas.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
 WITH stage_vendas AS (
     SELECT * FROM {{ ref('stage_vendas') }}
@@ -10,7 +10,7 @@ SELECT
         WHEN LENGTH(CAST(documento AS STRING)) = 18 THEN 'Vendas_CNPJ'
         ELSE 'Desconhecido'
     END AS tipo_cliente,
-    FORMAT('%#.3f', SUM(pricing)) AS valor_formatado
+    FORMAT('%#.3f', SUM(pricing)) AS valor_Total
 FROM
     `prj-lab-data-01.lab_data_dbt.stage_vendas`
 GROUP BY tipo_cliente
